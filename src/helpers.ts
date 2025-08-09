2ad0f768b4ab13ba573027d94131248af8ea5dae
Cyrus <tsuki2478@outlook.com>
2025-08-10 02:04:14 +0800

    feat: update staging-sei

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,9 +2,9 @@
 import { Address, BigDecimal, Bytes } from "@graphprotocol/graph-ts/index";
 import { Account, AccountCToken } from "../generated/schema";
 
-export const comptrollerAddress = "0x71034bf5eC0FAd7aEE81a213403c8892F3d8CAeE"; //UNITROLLER
-export const priceOracle = "0xD6a275072dceC8a319c0C7178951A0CF9DCC0447";
-export const rUSDCAddress = "0xC3c9e322F4aAe352ace79D0E62ADe3563fB86e87";
+export const comptrollerAddress = "0x16daCd28015C5168B62F27D934b2E11B925cb320"; //UNITROLLER
+export const priceOracle = "0xC3700e78665360972BdBb8c6D101e64dB0d252Aa";
+export const rUSDCAddress = "0xFb4c72D5A4239462326A5A005860a20CFeD094f4";
 
 export function exponentToBigDecimal(decimals: i32): BigDecimal {
   let bd = BigDecimal.fromString("1");
